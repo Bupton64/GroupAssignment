c21ff5c216c8198c5c9ae8bd93462541a27daca9
DESKTOP-HJTVN89\James-PC2 <jamesgwaddell@gmail.com>
2018-01-09 17:53:59 +1300

    Added menu for adventureMode

--- src/AdventureMode.java
+++ src/AdventureMode.java
@@ -32,6 +32,11 @@ public class AdventureMode extends GameEngine {
         lockDown = new boolean[1];
         lockUp = new boolean[1];
     }
+    private boolean pause = false;
+    private int cursorPosistionY = 440;
+    private boolean invMenu = false;
+    private boolean equMenu = false;
+    private boolean chaMenu = true;
 
 
 
@@ -69,45 +74,88 @@ public class AdventureMode extends GameEngine {
 
     @Override
     public void update(double dt) {
-        if(playerMan.getCombatActive()){
-            if(createCombat[0]) {
+        if(!pause) {
+            if (playerMan.getCombatActive()) {
+                if (createCombat[0]) {
 
-                combatMode = new Combat(playerMan);
-                createCombat[0] = false;
+                    combatMode = new Combat(playerMan);
+                    createCombat[0] = false;
+                }
+
+                state = GameState.CombatMode;
+            } else {
+                state = GameState.TravelMode;
             }
+            if (state == GameState.TravelMode) {
+                mapController.updateMap(playerMan, collisionDetector);
+                collisionDetector.updateCollision(playerMan, lockRight, lockLeft, lockUp, lockDown, playerMovement);
+                playerMovement.updateCharMovement(dt, lockRight[0], lockLeft[0], lockUp[0], lockDown[0], createCombat, playerMan);
+            } else if (state == GameState.CombatMode) {
+                combatMode.update(dt);
 
-            state = GameState.CombatMode;
-        }else{
-            state = GameState.TravelMode;
-        }
-        if(state == GameState.TravelMode) {
-            mapController.updateMap(playerMan, collisionDetector);
-            collisionDetector.updateCollision(playerMan, lockRight, lockLeft, lockUp, lockDown, playerMovement);
-            playerMovement.updateCharMovement(dt, lockRight[0], lockLeft[0], lockUp[0], lockDown[0],createCombat, playerMan);
-        }else if(state == GameState.CombatMode){
-            combatMode.update(dt);
-
+            }
         }
     }
 
 
     @Override
     public void paintComponent() {
-        clearBackground(800,600);
-        changeBackgroundColor(white);
-        if(state == GameState.TravelMode) {
-            mapController.drawMap(mGraphics); //< Draw the Map
-            playerMovement.drawCharMovement(mGraphics);//<Draw Player
-        }else if(state == GameState.CombatMode){
-            combatMode.paintComponent(mGraphics); //< Draw Combat
+        if (!pause) {
+            clearBackground(800, 600);
+            changeBackgroundColor(white);
+            if (state == GameState.TravelMode) {
+                mapController.drawMap(mGraphics); //< Draw the Map
+                playerMovement.drawCharMovement(mGraphics);//<Draw Player
+            } else if (state == GameState.CombatMode) {
+                combatMode.paintComponent(mGraphics); //< Draw Combat
+            }
+
+
+            //Debug Lines Remove on your version
+            changeColor(white);
+            drawText(50, 70, Integer.toString((int) playerMan.getMapPosX() / 10), "Times New Roman", 20);
+            drawText(50, 40, Integer.toString(collisionDetector.blocknum(playerMan)), "Times New Roman", 30);
+            drawText(50, 90, Integer.toString((int) playerMan.getMapPosY() / 10), "Times New Roman", 20);
+        }else{
+            if(chaMenu) {
+                clearBackground(800, 600);
+                changeColor(black);
+               // drawBoldText(100, 100, Integer.toString(cursorPosistionY));
+                drawBoldText(650, 450, "RESUME", "New Roman Times", 20);
+                drawBoldText(650, 480, "INVENTORY", "New Roman Times", 20);
+                drawBoldText(650, 510, "EQUIPMENT", "New Roman Times", 20);
+                drawBoldText(650, 540, "EXIT", "New Roman Times", 20);
+                drawBoldText(3, 595, "Current Map Number: "+ playerMan.getCurrentMapLocation(), "New Roman Times", 15);
+                drawBoldText(3, 30, playerMan.getName(), "Impact", 25);
+                changeColor(red);
+                drawBoldText(3, 50,"hp  "+ playerMan.getCurrentHP(), "New Roman Times", 15);
+                changeColor(black);
+                drawBoldText(3, 65, "xp "+ playerMan.getXPTotal(), "New Roman Times", 15);
+                changeColor(blue);
+                drawBoldText(3, 80, "lvl "+ playerMan.getCurrentLevel(), "New Roman Times", 15);
+
+                changeColor(blue);
+                drawBoldText(3, 575, "You need "+ playerMan.getXPToNextLevel()+" more xp to lvl up", "New Roman Times", 15);
+
+
+                changeColor(red);
+                drawSolidCircle(640, cursorPosistionY, 5);
+            }else if(invMenu){
+                clearBackground(800, 600);
+                changeColor(black);
+                drawBoldText(100, 100, "Add Inventory here");
+                drawBoldText(3, 580, "BACK [ESC]", "New Roman Times", 15);
+            }else if(equMenu){
+                clearBackground(800, 600);
+                changeColor(black);
+                drawBoldText(100, 100, "Add Equipment here");
+                drawBoldText(3, 580, "BACK [ESC]", "New Roman Times", 15);
+
+
+            }
+
+
         }
-
-
-        //Debug Lines Remove on your version
-        changeColor(white);
-        drawText(50,70,Integer.toString((int)playerMan.getMapPosX()/10),"Times New Roman",20);
-        drawText(50,40,Integer.toString(collisionDetector.blocknum(playerMan)),"Times New Roman",30);
-        drawText(50,90,Integer.toString((int)playerMan.getMapPosY()/10),"Times New Roman",20);
     }
 
     ///////////////////////////////////////////
@@ -143,6 +191,39 @@ public class AdventureMode extends GameEngine {
             combatMode.keyReleased(e);
 
         }
+        if(e.getKeyCode() == KeyEvent.VK_ESCAPE&&chaMenu == true){
+            pause = !pause;
+            chaMenu = true;
+        }
+        if(e.getKeyCode() == KeyEvent.VK_ESCAPE&&chaMenu == false){
+            chaMenu = true;
+        }
+        if(pause&&(e.getKeyCode() == KeyEvent.VK_DOWN)&&cursorPosistionY<510){
+            cursorPosistionY+=30;
+
+        }
+        if(pause&&(e.getKeyCode() == KeyEvent.VK_UP)&&cursorPosistionY>450){
+            cursorPosistionY-=30;
+
+        }
+        if(pause&&(e.getKeyCode() == KeyEvent.VK_ENTER)&&cursorPosistionY==440){
+            pause = !pause;
+
+        }
+        if(pause&&(e.getKeyCode() == KeyEvent.VK_ENTER)&&cursorPosistionY==530){
+            System.exit(23);
+        }
+        if(pause&&(e.getKeyCode() == KeyEvent.VK_ENTER)&&cursorPosistionY==470){
+            invMenu = true;
+            equMenu = false;
+            chaMenu = false;
+        }
+        if(pause&&(e.getKeyCode() == KeyEvent.VK_ENTER)&&cursorPosistionY==500){
+            equMenu = true;
+            invMenu = false;
+            chaMenu = false;
+        }
+
 
     }
 
