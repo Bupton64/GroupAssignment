3e3ab9bfe81e8f9ecf06d17e471fbe6719894a55
SLAIAX <1burgerboys@gmail.com>
2018-01-10 12:00:46 +1300

    More NPCS

--- src/npc_plains_D7.java
+++ src/npc_plains_D7.java
@@ -2,16 +2,16 @@ import java.awt.*;
 import java.awt.event.*;
 
 
-public class npc_plains_f8_byLog extends  NPC {
+public class npc_plains_D7 extends  NPC {
 
-    npc_plains_f8_byLog( ){
-        setName("Link");
-        spriteSheet = loadImage("chara3.png");
-        sprite = subImage(spriteSheet,520,0,56,72);
+    npc_plains_D7( ){
+        setName("Andrew");
+        spriteSheet = loadImage("chara4.png");
+        sprite = subImage(spriteSheet,520,288,56,72);
         setHeight(70);
         setWidth(50);
         setMapLocation(28);
-        setMapPos(650,85);
+        setMapPos(300,70);
         setHostile(false);
     }
 
