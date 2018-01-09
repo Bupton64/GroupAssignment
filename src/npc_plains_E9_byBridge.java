38dea37708d5f4c500083013570cacb678973568
SLAIAX <1burgerboys@gmail.com>
2018-01-10 11:24:21 +1300

    Added 3 NPCS into E9 ... or in James talk "Yea BOIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII"

---
+++ src/npc_plains_E9_byBridge.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class npc_plains_E9_byBridge extends  NPC {
+
+    npc_plains_E9_byBridge( ){
+        setName("Sally");
+        spriteSheet = loadImage("chara5.png");
+        sprite = subImage(spriteSheet,364,144,56,72);
+        setHeight(70);
+        setWidth(50);
+        setMapLocation(21);
+        setMapPos(475,200);
+        setHostile(false);
+    }
+
+    @Override
+    public void setUpCollision(Collision collisionDetector,extraFunctions map){
+        collisionDetector.addBoxCollision(((int)getMapPosX()/ 10 - 2),((int)getMapPosY()/10 - 5),((int)getWidth()/10 - 2),((int)getHeight()/10 - 2),map.isFlicker());
+    }
+
+    @Override
+    public void npcAction(){
+
+    }
+
+
+
+
+
+}
