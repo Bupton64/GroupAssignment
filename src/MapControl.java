a5ad330db48750f8cccaf45ba8c8e7ebb21cadcc
ZLamb101 <zanelamb@live.com>
2018-01-11 13:20:01 +1300

    map control edit for removal of forest level

--- src/MapControl.java
+++ src/MapControl.java
@@ -66,13 +66,13 @@ public class MapControl extends extraFunctions {
 
             if (!firstMap) {
                 currentMap.setUpCollision(collisionDetector);
-                for(int i = 0; i < numOfNpc; i++) {
+                for (int i = 0; i < numOfNpc; i++) {
                     mapNpcs[i].setUpCollision(collisionDetector, currentMap);
                 }
             }
 
-            for(int i = 0; i < numOfNpc;i++){
-                mapNpcs[i] =  new npc_empty();
+            for (int i = 0; i < numOfNpc; i++) {
+                mapNpcs[i] = new npc_empty();
             }
 
             swapMap = false;
@@ -225,134 +225,136 @@ public class MapControl extends extraFunctions {
                         currentMap = new plains_I9();
                         break;
                 }
-
-            } else if (levelController == Level.Forest) {
-                switch ((int) playerMan.getCurrentMapLocation()) {
-                    case 0:
-                        currentMap = new forest_A4();
-                        break;
-                    case 1:
-                        currentMap = new forest_A5();
-                        break;
-                    case 2:
-                        currentMap = new forest_B3();
-                        break;
-                    case 3:
-                        currentMap = new forest_B4();
-                        break;
-                    case 4:
-                        currentMap = new forest_B5();
-                        break;
-                    case 5:
-                        currentMap = new forest_B6();
-                        break;
-                    case 6:
-                        currentMap = new forest_C2();
-                        break;
-                    case 7:
-                        currentMap = new forest_C3();
-                        break;
-                    case 8:
-                        currentMap = new forest_C4();
-                        break;
-                    case 9:
-                        currentMap = new forest_C5();
-                        break;
-                    case 10:
-                        currentMap = new forest_C6();
-                        break;
-                    case 11:
-                        currentMap = new forest_C7();
-                        break;
-                    case 12:
-                        currentMap = new forest_D1();
-                        break;
-                    case 13:
-                        currentMap = new forest_D2();
-                        break;
-                    case 14:
-                        currentMap = new forest_D3();
-                        break;
-                    case 15:
-                        currentMap = new forest_D4();
-                        break;
-                    case 16:
-                        currentMap = new forest_D5();
-                        break;
-                    case 17:
-                        currentMap = new forest_D6();
-                        break;
-                    case 18:
-                        currentMap = new forest_D7();
-                        break;
-                    case 19:
-                        currentMap = new forest_D8();
-                        break;
-                    case 20:
-                        currentMap = new forest_E1();
-                        break;
-                    case 21:
-                        currentMap = new forest_E2();
-                        break;
-                    case 22:
-                        currentMap = new forest_E3();
-                        break;
-                    case 23:
-                        currentMap = new forest_E4();
-                        break;
-                    case 24:
-                        currentMap = new forest_E5();
-                        break;
-                    case 25:
-                        currentMap = new forest_E6();
-                        break;
-                    case 26:
-                        currentMap = new forest_E7();
-                        break;
-                    case 27:
-                        currentMap = new forest_E8();
-                        break;
-                    case 28:
-                        currentMap = new forest_F2();
-                        break;
-                    case 29:
-                        currentMap = new forest_F3();
-                        break;
-                    case 30:
-                        currentMap = new forest_F4();
-                        break;
-                    case 31:
-                        currentMap = new forest_F5();
-                        break;
-                    case 32:
-                        currentMap = new forest_F6();
-                        break;
-                    case 33:
-                        currentMap = new forest_F7();
-                        break;
-                    case 34:
-                        currentMap = new forest_G3();
-                        break;
-                    case 35:
-                        currentMap = new forest_G4();
-                        break;
-                    case 36:
-                        currentMap = new forest_G5();
-                        break;
-                    case 37:
-                        currentMap = new forest_G6();
-                        break;
-                    case 38:
-                        currentMap = new forest_H4();
-                        break;
-                    case 39:
-                        currentMap = new forest_H5();
-                        break;
-
-                }
-
             }
         }
+
+//            } else if (levelController == Level.Forest) {
+//                switch ((int) playerMan.getCurrentMapLocation()) {
+//                    case 0:
+//                        currentMap = new forest_A4();
+//                        break;
+//                    case 1:
+//                        currentMap = new forest_A5();
+//                        break;
+//                    case 2:
+//                        currentMap = new forest_B3();
+//                        break;
+//                    case 3:
+//                        currentMap = new forest_B4();
+//                        break;
+//                    case 4:
+//                        currentMap = new forest_B5();
+//                        break;
+//                    case 5:
+//                        currentMap = new forest_B6();
+//                        break;
+//                    case 6:
+//                        currentMap = new forest_C2();
+//                        break;
+//                    case 7:
+//                        currentMap = new forest_C3();
+//                        break;
+//                    case 8:
+//                        currentMap = new forest_C4();
+//                        break;
+//                    case 9:
+//                        currentMap = new forest_C5();
+//                        break;
+//                    case 10:
+//                        currentMap = new forest_C6();
+//                        break;
+//                    case 11:
+//                        currentMap = new forest_C7();
+//                        break;
+//                    case 12:
+//                        currentMap = new forest_D1();
+//                        break;
+//                    case 13:
+//                        currentMap = new forest_D2();
+//                        break;
+//                    case 14:
+//                        currentMap = new forest_D3();
+//                        break;
+//                    case 15:
+//                        currentMap = new forest_D4();
+//                        break;
+//                    case 16:
+//                        currentMap = new forest_D5();
+//                        break;
+//                    case 17:
+//                        currentMap = new forest_D6();
+//                        break;
+//                    case 18:
+//                        currentMap = new forest_D7();
+//                        break;
+//                    case 19:
+//                        currentMap = new forest_D8();
+//                        break;
+//                    case 20:
+//                        currentMap = new forest_E1();
+//                        break;
+//                    case 21:
+//                        currentMap = new forest_E2();
+//                        break;
+//                    case 22:
+//                        currentMap = new forest_E3();
+//                        break;
+//                    case 23:
+//                        currentMap = new forest_E4();
+//                        break;
+//                    case 24:
+//                        currentMap = new forest_E5();
+//                        break;
+//                    case 25:
+//                        currentMap = new forest_E6();
+//                        break;
+//                    case 26:
+//                        currentMap = new forest_E7();
+//                        break;
+//                    case 27:
+//                        currentMap = new forest_E8();
+//                        break;
+//                    case 28:
+//                        currentMap = new forest_F2();
+//                        break;
+//                    case 29:
+//                        currentMap = new forest_F3();
+//                        break;
+//                    case 30:
+//                        currentMap = new forest_F4();
+//                        break;
+//                    case 31:
+//                        currentMap = new forest_F5();
+//                        break;
+//                    case 32:
+//                        currentMap = new forest_F6();
+//                        break;
+//                    case 33:
+//                        currentMap = new forest_F7();
+//                        break;
+//                    case 34:
+//                        currentMap = new forest_G3();
+//                        break;
+//                    case 35:
+//                        currentMap = new forest_G4();
+//                        break;
+//                    case 36:
+//                        currentMap = new forest_G5();
+//                        break;
+//                    case 37:
+//                        currentMap = new forest_G6();
+//                        break;
+//                    case 38:
+//                        currentMap = new forest_H4();
+//                        break;
+//                    case 39:
+//                        currentMap = new forest_H5();
+//                        break;
+//
+//                }
+//
+//            }
+//        }
         for(int i = 0; i < numOfNpc; i++) {
             mapNpcs[i].setUpCollision(collisionDetector, currentMap);
         }
