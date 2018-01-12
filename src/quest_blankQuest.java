a1d31ae8fa042634db08527b6cafe999695e212f
ZLamb101 <zanelamb@live.com>
2018-01-12 16:33:05 +1300

    added a blank quest for starting off

---
+++ src/quest_blankQuest.java
@@ -0,0 +1,27 @@
+
+import java.awt.*;
+
+public class quest_blankQuest extends Quest {
+
+
+
+    private int killCount; //<Amount of monsters killed
+
+    quest_blankQuest(){
+        setState(questState.completedQuest);
+
+    }
+
+
+
+    public void drawQuest(Graphics2D g){
+
+    }
+
+    public void updateQuest(){
+
+    }
+
+
+}
+
