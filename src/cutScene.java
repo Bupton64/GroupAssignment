47cd3ca35e5412fc347f5184016cdba99c59fc1c
SLAIAX <1burgerboys@gmail.com>
2018-01-13 22:22:41 +1300

    Made a cutscene class in preparation... nothing in it currently :P

---
+++ src/cutScene.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+
+public class cutScene extends extraFunctions {
+    //WON'T BE USED FOR A WHILE
+
+    /*
+     * Loads all required images
+     */
+    cutScene(){
+
+    }
+    /*
+     * Draws the whole cutscene
+     */
+    public void drawCutScene(Graphics2D g){
+
+    }
+}
