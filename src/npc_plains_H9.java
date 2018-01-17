f59705b83a93f024dc7f01797818f42c92ae6d8f
SLAIAX <1burgerboys@gmail.com>
2018-01-17 23:19:41 +1300

    re-adjusted to allow for Link the blacksmith assistant

--- src/npc_plains_H9.java
+++ src/npc_plains_H9.java
@@ -56,7 +56,7 @@ public class npc_plains_H9 extends  NPC {
         Dialogue d2 = new Dialogue(null,true,true,"Bjarne! I've been expecting you ever since I heard you had arrived.","Let me guess, you're looking for a weapon? Lucky for you I have an","abundance. Now I can't exactly give them away, but considering the","circumstances I'll sell everything to you at a lowered price.");
         listTwo = d2;
 
-        Dialogue d3 = new Dialogue(null,true,true,"Ahhhh good choice! If there's ever anything else you need please","feel free to come back and browse my wares. I always strive to make ","more powerful weaponry and refresh my inventory so you're bound to","find something of use!");
+        Dialogue d3 = new Dialogue(null,true,true,"Ahhhh good choice! If there's ever anything else you need please","feel free to come back and browse my wares, just talk to my assistant","Link. I always strive to make more powerful weaponry and refresh ","my inventory so you're bound to find something of use!");
         listThree = d3;
 
     }
