00e827107151e0165cb125445c435f454d266ecc
SLAIAX <1burgerboys@gmail.com>
2018-01-24 18:58:56 +1300

    cutscene with a little change. Telling me to merge, hopefully no one edited it

--- src/cutScene.java
+++ src/cutScene.java
@@ -8,10 +8,13 @@ public class cutScene extends extraFunctions {
     Image spriteSheet;
     Image spriteSheet2;
     Image spriteSheet3;
+    Image dialogueBack;
+    Image dialogueBackSheet;
     Image spriteDown[];
     Image spriteDown2[];
     Image spriteDown3[];
     Image wizardDown[];
+    Image wizardSpin[];
     double timer;
     double timePast;
     boolean back;
@@ -37,7 +40,7 @@ public class cutScene extends extraFunctions {
         background = loadImage("intro_cutscene.png");
         backgroundAlt = loadImage("intro_cutscene2.png");
         timer = 0;
-        timePast = 0;
+        timePast = 9999999;
         back = true;
         render = true;
         state = introState.text;
@@ -48,6 +51,9 @@ public class cutScene extends extraFunctions {
         spriteDown2 = new Image[3];
         spriteDown3 = new Image[3];
         wizardDown = new Image[3];
+        wizardSpin = new Image[4];
+        dialogueBackSheet = loadImage("dialogue_boxes.png");
+        dialogueBack = subImage(dialogueBackSheet,20,20,470,100);
         posX = -800;
         posY = -600;
         runSpeed = 0;
@@ -55,14 +61,16 @@ public class cutScene extends extraFunctions {
         runSpeedLeft = 0;
         flameChange = 0;
         height = 0;
-        wizardPosX = 130;
+        wizardPosX = 120;
         wizardPosY = -74;
         for(int i =0; i < 3;i++){
             spriteDown[i] = subImage(spriteSheet,156 + (52 * i), 0,52,72);
             spriteDown2[i] = subImage(spriteSheet,312 + (52 * i), 0,52,72);
             spriteDown3[i] = subImage(spriteSheet2,312 + (52 * i), 0,52,72);
             wizardDown[i] = subImage(spriteSheet3,468 + (52 * i), 288,52,72);
+            wizardSpin[i] = subImage(spriteSheet3,530, 288 + (72*i),52,72);
         }
+        wizardSpin[3] = subImage(spriteSheet3,530, 504,52,72);
     }
     /*
      * Updates the timer
@@ -198,6 +206,20 @@ public class cutScene extends extraFunctions {
             if((posY >= 0) && (wizardPosY < 50)){
                 wizardPosY+=1;
                 drawImage(wizardDown[flameChange % 3], wizardPosX, wizardPosY, g);
+                timePast = timer + 5;
+            }
+            if((posY >= 0) && (wizardPosY >= 50)){
+                if(timePast > timer) {
+                    wizardPosY += 1;
+                    changeColor(white, g);
+                    drawImage(wizardDown[1], wizardPosX, 50, g);
+                    drawImage(dialogueBack, 90, 400, 620, 165, g);
+                    drawText(110, 450, "MWA HA HA HA! There's no stopping me now! Once I gain control", "Times New Roman", 20, g);
+                    drawText(110, 475, "of the Seven Crystals of the South, my power will be un-matchable!", "Times New Roman", 20, g);
+                }
+            }
+            if((timePast < timer) && (posY>=0)){
+                drawImage(wizardSpin[flameChange % 4], wizardPosX, 50, g);
             }
         }
     }
