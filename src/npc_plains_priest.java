a1872973154600d49db2e82554ba1030faf17925
SLAIAX <1burgerboys@gmail.com>
2018-01-27 19:34:37 +1300

    Priest dialogue edited

--- src/npc_plains_priest.java
+++ src/npc_plains_priest.java
@@ -48,12 +48,12 @@ public class npc_plains_priest extends  NPC {
 
     public void initDialogue() {
 
-        Dialogue d1 = new Dialogue(null,false,true,"Hello, little man, you seem weak.... ","I would consider changing your Faith before the end comes","","");
+        Dialogue d1 = new Dialogue(null,false,true,"Hello little man, you seem weak... ","I would consider changing your faith before the end comes!","","");
         listOne = d1;
 
-        Dialogue d4 = new Dialogue(null,true,true,"Or will you try to stop me? hahaha","","","");
-        Dialogue d3 = new Dialogue(d4,false,false, "The dark lord is near, you have lost,","And now your friend Julian shall die.","","");
-        Dialogue d2 = new Dialogue(d3,false,false, "Hello Again little man. ","I see you found my Book, well no problem the time has come for action","","");
+        Dialogue d4 = new Dialogue(null,true,true,"Or will you try to stop me? Mwa ha ha ha","","","");
+        Dialogue d3 = new Dialogue(d4,false,false, "The dark lord is near, you have lost","and now your friend Julian shall die.","","");
+        Dialogue d2 = new Dialogue(d3,false,false, "Hello again little man. ","I see you found my book, well no problem, the time has come for action!","","");
         listTwo = d2;
 
 
