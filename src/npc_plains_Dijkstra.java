025d5c2254e36167306cedfcb872e27d4577f993
ZLamb101 <zanelamb@live.com>
2018-01-30 22:22:22 +1300

    fixed teleport

--- src/npc_plains_Dijkstra.java
+++ src/npc_plains_Dijkstra.java
@@ -121,7 +121,7 @@ public class npc_plains_Dijkstra extends  NPC {
             case 25:
                 currentDialogue = listThree;
                 return 25;
-            case 31:
+            case 30:
                 return 95;
             default:
                 return 0;
@@ -146,8 +146,8 @@ public class npc_plains_Dijkstra extends  NPC {
                         case 24:
                             questStage = 25;
                             break;
-                        case 30:
-                            questStage = 31;
+                        case 29:
+                            questStage = 30;
                             break;
                     }
                 }
