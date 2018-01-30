a997878e18181bfb472753e94c8f50850e5078aa
Bupton64 <bupton@hotmail.co.nz>
2018-01-31 11:53:59 +1300

    Maybe this time?

--- src/ability_toxicity.java
+++ src/ability_toxicity.java
@@ -7,11 +7,11 @@ public class ability_toxicity extends Ability {
         this.setDefense(0);
         this.setLuck(0);
         this.setStrength(0);
-        this.setMagic(false);
+        this.setMagic(true);
         this.setEnergyCost(3);
         this.setActive(false);
         this.setType(AbilityType.curse);
-        this.setDisplayString("");
+        this.setToolTip("Boil the blood of your foe");
     }
 
     @Override
@@ -25,6 +25,9 @@ public class ability_toxicity extends Ability {
             toxic.setLastStatusDuration(4);
             int rand = (int) (Math.random() * 8);
             toxic.setDamageOverTime(rand + user.getLevel() * 3);
+            this.setDisplayString("Your foe's blood boils!");
+        } else{
+            this.setDisplayString("Nothing happened!");
         }
         return toxic;
     }
