926100bd1379942d20285b6925015cd38fb3a366
SLAIAX <1burgerboys@gmail.com>
2018-01-31 19:13:17 +1300

    pngs

--- src/monster_BOSS_priest.java
+++ src/monster_BOSS_priest.java
@@ -24,7 +24,7 @@ public class monster_BOSS_priest extends Monster {
         setName("Priest");
         setEnergy(0);
 
-        Image spriteSheet = loadImage("priest.png");
+        Image spriteSheet = loadImage("monster_priest.png");
         Image sprite = subImage(spriteSheet,364,72,56,72);
         setSprite(sprite);
         setSpriteWidth(200);
