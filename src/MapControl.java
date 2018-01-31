ad4171afb3cf359f6299b3c4d585eb8691b939ef
SLAIAX <1burgerboys@gmail.com>
2018-01-31 14:13:39 +1300

    Sevar Grave fix, sign fixes and extra dialogue added

--- src/MapControl.java
+++ src/MapControl.java
@@ -168,6 +168,10 @@ public class MapControl extends extraFunctions {
                     currentMap = new plains_E5();
                     mapNpcs[0] = new npc_plains_SevarsWifeGrave();
                     numOfNpc = 1;
+                    if(playerMan.getQuestStage() == 33) {
+                        mapNpcs[1] = new npc_plains_SevarsGrave();
+                        numOfNpc = 2;
+                    }
                     if(playerMan.getQuestStage() < 16) {
                         mapNpcs[1] = new npc_wizard(400, 250);
                         numOfNpc = 2;
